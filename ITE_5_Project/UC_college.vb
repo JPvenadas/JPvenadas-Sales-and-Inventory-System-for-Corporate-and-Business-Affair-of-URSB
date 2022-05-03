d19e56bb50961d7e9fe520dce60c5393ff5aa0a3
Jp Venadas <jaypheevenadas18@gmail.com>
2022-05-03 08:06:29 +0800

    update college function

--- ITE_5_Project/UC_college.vb
+++ ITE_5_Project/UC_college.vb
@@ -1,4 +1,5 @@
 ﻿Public Class UC_college
+    Dim previousId
     Private Sub UC_college_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Refreshdata()
     End Sub
@@ -14,6 +15,34 @@
     End Sub
 
     Private Sub BTN_save_Click(sender As Object, e As EventArgs) Handles BTN_save.Click
+        If LBL_operation.Text = "Add a College" Then
+            ErrorTrappingadd()
+        Else
+            ErrorTrappingupdate()
+        End If
+    End Sub
+
+    Private Sub DGV_products_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DGV_colleges.CellClick
+        Enable()
+        Dim row = DGV_colleges.Rows(e.RowIndex)
+        TXT_collegeID.Text = row.Cells(0).Value
+        previousId = row.Cells(0).Value
+        TXT_collegeName.Text = row.Cells(1).Value
+        LBL_operation.Text = "Edit a College"
+    End Sub
+
+    Sub Clearinputs()
+        TXT_collegeID.Text = ""
+        TXT_collegeName.Text = ""
+    End Sub
+    Sub Enable()
+        Clearinputs()
+        TXT_collegeName.Enabled = True
+        TXT_collegeID.Enabled = True
+        BTN_clear.Enabled = True
+        BTN_save.Enabled = True
+    End Sub
+    Sub ErrorTrappingadd()
         openCon()
         cmd.CommandText = "select * from tbl_colleges where collegeID = @ID"
         With cmd.Parameters
@@ -38,25 +67,31 @@
         Refreshdata()
     End Sub
 
-    Private Sub DGV_products_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DGV_colleges.CellClick
-        Clearinputs()
-        Dim row = DGV_colleges.Rows(e.RowIndex)
-        TXT_collegeID = row.Cells(0).Value
-        TXT_collegeName = row.Cells(1).Value
-        Enable()
-        LBL_operation.Text = "Edit a College"
-    End Sub
+    Sub ErrorTrappingupdate()
+        openCon()
+        cmd.CommandText = "select * from tbl_colleges where collegeID = @ID"
+        With cmd.Parameters
+            .Clear()
+            .AddWithValue("ID", TXT_collegeID.Text)
+        End With
+        cmd.ExecuteNonQuery()
 
-    Sub Clearinputs()
-        TXT_collegeID.Text = ""
-        TXT_collegeName.Text = ""
-    End Sub
-    Sub Enable()
-        Clearinputs()
-        TXT_collegeName.Enabled = True
-        TXT_collegeID.Enabled = True
-        BTN_clear.Enabled = True
-        BTN_save.Enabled = True
+        dr = cmd.ExecuteReader
+        If dr.HasRows Then
+            dr.Read()
+            If dr(0) <> previousId Then
+                MsgBox("The College Department is already on the list", vbOKOnly + vbExclamation, "Updating Error")
+                con.Close()
+                Exit Sub
+            End If
+        End If
+        con.Close()
+        If TXT_collegeID.Text = "" And TXT_collegeName.Text = "" Then
+            MsgBox("All fields are required", vbOKOnly + vbExclamation, "Saving Error")
+            Exit Sub
+        End If
+        UpdateCollege()
+        Refreshdata()
     End Sub
 
     Sub AddCollege()
@@ -66,6 +101,18 @@
             .Clear()
             .AddWithValue("id", TXT_collegeID.Text)
             .AddWithValue("name", TXT_collegeName.Text)
+        End With
+        cmd.ExecuteNonQuery()
+        con.Close()
+    End Sub
+    Sub UpdateCollege()
+        openCon()
+        cmd.CommandText = "Update tbl_colleges set collegeID= @ID, collegeName = @name where collegeID = @prevID"
+        With cmd.Parameters
+            .Clear()
+            .AddWithValue("ID", TXT_collegeID.Text)
+            .AddWithValue("name", TXT_collegeName.Text)
+            .AddWithValue("prevID", previousId)
         End With
         cmd.ExecuteNonQuery()
         con.Close()
