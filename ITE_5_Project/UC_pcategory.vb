594ebaf19edb9fcfc7e85936c9937b54c442629d
Jp Venadas <jaypheevenadas18@gmail.com>
2022-05-03 14:34:51 +0800

    update product category function

--- ITE_5_Project/UC_pcategory.vb
+++ ITE_5_Project/UC_pcategory.vb
@@ -4,7 +4,11 @@
         RefreshData()
     End Sub
     Private Sub BTN_save_Click(sender As Object, e As EventArgs) Handles BTN_save.Click
-        ErrorTrappingAdd()
+        If LBL_operation.Text = "Add a Product Category" Then
+            ErrorTrappingAdd()
+        Else
+            ErrorTrappingUpdate()
+        End If
     End Sub
     Private Sub BTN_clear_Click(sender As Object, e As EventArgs) Handles BTN_clear.Click
         If BTN_clear.Text = "Delete" Then
@@ -55,11 +59,46 @@
         con.Close()
         Addcategory()
     End Sub
+    Sub ErrorTrappingUpdate()
+        If TXT_catID.Text = "" Then
+            MsgBox("Input the Category Name", vbOKOnly + vbExclamation, "Error Saving")
+            Exit Sub
+        End If
+        openCon()
+        cmd.CommandText = "select * from tbl_categories where ProductCategory = @cat"
+        cmd.Parameters.Clear()
+        cmd.Parameters.AddWithValue("cat", TXT_catID.Text)
+        cmd.ExecuteNonQuery()
+        dr = cmd.ExecuteReader
+        If dr.HasRows Then
+            dr.Read()
+            If dr(0) <> prevcat Then
+                MsgBox("The Product Category is already on the list", vbOKOnly + vbExclamation, "Error Updating")
+                TXT_catID.Text = prevcat
+                con.Close()
+                Exit Sub
+            End If
+        End If
+        con.Close()
+        Updatecategory()
+    End Sub
     Sub Addcategory()
         openCon()
         cmd.CommandText = "insert into tbl_categories values(@cat)"
         cmd.Parameters.Clear()
         cmd.Parameters.AddWithValue("cat", TXT_catID.Text)
+        cmd.ExecuteNonQuery()
+        con.Close()
+        RefreshData()
+    End Sub
+    Sub Updatecategory()
+        openCon()
+        cmd.CommandText = "Update tbl_categories set ProductCategory = @cat where ProductCategory = @prevcat"
+        With cmd.Parameters
+            .Clear()
+            .AddWithValue("cat", TXT_catID.Text)
+            .AddWithValue("prevcat", prevcat)
+        End With
         cmd.ExecuteNonQuery()
         con.Close()
         RefreshData()
