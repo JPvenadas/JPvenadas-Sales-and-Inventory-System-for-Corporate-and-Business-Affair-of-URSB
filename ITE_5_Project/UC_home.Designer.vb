f2e16a94011454620f7cb831517b8237a3987938
Jp Venadas <jaypheevenadas18@gmail.com>
2022-05-06 22:42:46 +0800

    structure of home interface

--- ITE_5_Project/UC_home.Designer.vb
+++ ITE_5_Project/UC_home.Designer.vb
@@ -22,16 +22,171 @@ Partial Class UC_home
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.MaterialCard1 = New MaterialSkin.Controls.MaterialCard()
+        Me.MaterialCard2 = New MaterialSkin.Controls.MaterialCard()
+        Me.MaterialCard3 = New MaterialSkin.Controls.MaterialCard()
+        Me.MaterialCard4 = New MaterialSkin.Controls.MaterialCard()
+        Me.MaterialCard5 = New MaterialSkin.Controls.MaterialCard()
+        Me.MaterialCard6 = New MaterialSkin.Controls.MaterialCard()
+        Me.MaterialCard7 = New MaterialSkin.Controls.MaterialCard()
+        Me.MaterialCard8 = New MaterialSkin.Controls.MaterialCard()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(29, Byte), Integer), CType(CType(75, Byte), Integer), CType(CType(158, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Location = New System.Drawing.Point(375, 67)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(602, 209)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.BackgroundImage = Global.ITE_5_Project.My.Resources.Resources.Wallpaper
+        Me.Panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Panel2.Location = New System.Drawing.Point(274, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(328, 209)
+        Me.Panel2.TabIndex = 0
+        '
+        'MaterialCard1
+        '
+        Me.MaterialCard1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.MaterialCard1.Depth = 0
+        Me.MaterialCard1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.MaterialCard1.Location = New System.Drawing.Point(14, 9)
+        Me.MaterialCard1.Margin = New System.Windows.Forms.Padding(14)
+        Me.MaterialCard1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialCard1.Name = "MaterialCard1"
+        Me.MaterialCard1.Padding = New System.Windows.Forms.Padding(14)
+        Me.MaterialCard1.Size = New System.Drawing.Size(963, 41)
+        Me.MaterialCard1.TabIndex = 1
+        '
+        'MaterialCard2
+        '
+        Me.MaterialCard2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.MaterialCard2.Depth = 0
+        Me.MaterialCard2.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.MaterialCard2.Location = New System.Drawing.Point(375, 293)
+        Me.MaterialCard2.Margin = New System.Windows.Forms.Padding(14)
+        Me.MaterialCard2.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialCard2.Name = "MaterialCard2"
+        Me.MaterialCard2.Padding = New System.Windows.Forms.Padding(14)
+        Me.MaterialCard2.Size = New System.Drawing.Size(144, 94)
+        Me.MaterialCard2.TabIndex = 2
+        '
+        'MaterialCard3
+        '
+        Me.MaterialCard3.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.MaterialCard3.Depth = 0
+        Me.MaterialCard3.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.MaterialCard3.Location = New System.Drawing.Point(527, 293)
+        Me.MaterialCard3.Margin = New System.Windows.Forms.Padding(14)
+        Me.MaterialCard3.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialCard3.Name = "MaterialCard3"
+        Me.MaterialCard3.Padding = New System.Windows.Forms.Padding(14)
+        Me.MaterialCard3.Size = New System.Drawing.Size(144, 94)
+        Me.MaterialCard3.TabIndex = 2
+        '
+        'MaterialCard4
+        '
+        Me.MaterialCard4.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.MaterialCard4.Depth = 0
+        Me.MaterialCard4.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.MaterialCard4.Location = New System.Drawing.Point(680, 293)
+        Me.MaterialCard4.Margin = New System.Windows.Forms.Padding(14)
+        Me.MaterialCard4.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialCard4.Name = "MaterialCard4"
+        Me.MaterialCard4.Padding = New System.Windows.Forms.Padding(14)
+        Me.MaterialCard4.Size = New System.Drawing.Size(144, 94)
+        Me.MaterialCard4.TabIndex = 2
+        '
+        'MaterialCard5
+        '
+        Me.MaterialCard5.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.MaterialCard5.Depth = 0
+        Me.MaterialCard5.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.MaterialCard5.Location = New System.Drawing.Point(833, 293)
+        Me.MaterialCard5.Margin = New System.Windows.Forms.Padding(14)
+        Me.MaterialCard5.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialCard5.Name = "MaterialCard5"
+        Me.MaterialCard5.Padding = New System.Windows.Forms.Padding(14)
+        Me.MaterialCard5.Size = New System.Drawing.Size(144, 94)
+        Me.MaterialCard5.TabIndex = 2
+        '
+        'MaterialCard6
+        '
+        Me.MaterialCard6.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.MaterialCard6.Depth = 0
+        Me.MaterialCard6.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.MaterialCard6.Location = New System.Drawing.Point(14, 58)
+        Me.MaterialCard6.Margin = New System.Windows.Forms.Padding(14)
+        Me.MaterialCard6.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialCard6.Name = "MaterialCard6"
+        Me.MaterialCard6.Padding = New System.Windows.Forms.Padding(14)
+        Me.MaterialCard6.Size = New System.Drawing.Size(344, 119)
+        Me.MaterialCard6.TabIndex = 3
+        '
+        'MaterialCard7
+        '
+        Me.MaterialCard7.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.MaterialCard7.Depth = 0
+        Me.MaterialCard7.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.MaterialCard7.Location = New System.Drawing.Point(14, 188)
+        Me.MaterialCard7.Margin = New System.Windows.Forms.Padding(14)
+        Me.MaterialCard7.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialCard7.Name = "MaterialCard7"
+        Me.MaterialCard7.Padding = New System.Windows.Forms.Padding(14)
+        Me.MaterialCard7.Size = New System.Drawing.Size(344, 355)
+        Me.MaterialCard7.TabIndex = 3
+        '
+        'MaterialCard8
+        '
+        Me.MaterialCard8.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.MaterialCard8.Depth = 0
+        Me.MaterialCard8.ForeColor = System.Drawing.Color.FromArgb(CType(CType(222, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.MaterialCard8.Location = New System.Drawing.Point(375, 396)
+        Me.MaterialCard8.Margin = New System.Windows.Forms.Padding(14)
+        Me.MaterialCard8.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialCard8.Name = "MaterialCard8"
+        Me.MaterialCard8.Padding = New System.Windows.Forms.Padding(14)
+        Me.MaterialCard8.Size = New System.Drawing.Size(602, 147)
+        Me.MaterialCard8.TabIndex = 4
         '
         'UC_home
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
+        Me.Controls.Add(Me.MaterialCard8)
+        Me.Controls.Add(Me.MaterialCard7)
+        Me.Controls.Add(Me.MaterialCard6)
+        Me.Controls.Add(Me.MaterialCard5)
+        Me.Controls.Add(Me.MaterialCard4)
+        Me.Controls.Add(Me.MaterialCard3)
+        Me.Controls.Add(Me.MaterialCard2)
+        Me.Controls.Add(Me.MaterialCard1)
+        Me.Controls.Add(Me.Panel1)
         Me.Name = "UC_home"
-        Me.Size = New System.Drawing.Size(729, 480)
+        Me.Size = New System.Drawing.Size(1048, 543)
+        Me.Panel1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents MaterialCard1 As MaterialSkin.Controls.MaterialCard
+    Friend WithEvents MaterialCard2 As MaterialSkin.Controls.MaterialCard
+    Friend WithEvents MaterialCard3 As MaterialSkin.Controls.MaterialCard
+    Friend WithEvents MaterialCard4 As MaterialSkin.Controls.MaterialCard
+    Friend WithEvents MaterialCard5 As MaterialSkin.Controls.MaterialCard
+    Friend WithEvents MaterialCard6 As MaterialSkin.Controls.MaterialCard
+    Friend WithEvents MaterialCard7 As MaterialSkin.Controls.MaterialCard
+    Friend WithEvents MaterialCard8 As MaterialSkin.Controls.MaterialCard
+    Friend WithEvents Panel2 As Panel
 End Class
