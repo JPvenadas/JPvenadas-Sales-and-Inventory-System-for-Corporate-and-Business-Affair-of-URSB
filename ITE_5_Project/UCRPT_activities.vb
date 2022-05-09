d53455002283c4317c94089c79621c48d63d5180
Jp Venadas <jaypheevenadas18@gmail.com>
2022-05-10 07:20:19 +0800

    transaction and activities report

--- ITE_5_Project/UCRPT_activities.vb
+++ ITE_5_Project/UCRPT_activities.vb
@@ -1,3 +1,7 @@
 ﻿Public Class UCRPT_activities
-
+    Private Sub UCRPT_activities_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.tbl_activitiesTableAdapter.Fill(Me.db_inventoryDataSet.tbl_activities)
+        RPTV_activities.RefreshReport()
+        RPTV_activities.SetDisplayMode(Microsoft.Reporting.WinForms.DisplayMode.PrintLayout)
+    End Sub
 End Class
--- ITE_5_Project/UCRPT_transactions.vb
+++ ITE_5_Project/UCRPT_transactions.vb
@@ -1,3 +1,7 @@
 ﻿Public Class UCRPT_transactions
-
+    Private Sub UCRPT_transactions_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.tbl_transactionsTableAdapter.Fill(Me.db_inventoryDataSet.tbl_transactions)
+        RPTV_transactions.RefreshReport()
+        RPTV_transactions.SetDisplayMode(Microsoft.Reporting.WinForms.DisplayMode.PrintLayout)
+    End Sub
 End Class
